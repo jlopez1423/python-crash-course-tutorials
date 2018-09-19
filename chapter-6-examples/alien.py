0af45fe76c19c1d8cfa12ef071a9680d470477f5
Jose <jalopez1423@gmail.com>
2018-09-18 21:08:37 -0700

    alien dictionary example

--- chapter-6-examples/alien.py
+++ chapter-6-examples/alien.py
@@ -1,4 +1,46 @@
-alien_0 = {'color': 'green', 'points': 5}
+## alien position
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
 
-print(alien_0['color'])
-print(alien_0['points'])
+# Move the alien to the right
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # this must be a fast alien
+    x_increment = 3
+
+# the new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
+
+# alien_0 = {'color': 'green', 'points': 5}
+
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+# new_points = alien_0['points']
+# print("you just earned " + str(new_points) + " points!")
+
+# adding items to dictionary
+# alien_0['x_position'] = 0
+# alien_0['y_position'] = 25
+# print(alien_0)
+
+##starting with an empty dictionary
+# alien_0 = {}
+
+# alien_0['color'] = 'green'
+# alien_0['points'] = 5
+
+# print (alien_0)
+
+##Modifying value in  dictionary
+# alien_0 = {'color': 'green'}
+# print('The alien is ' + alien_0['color'] + '.')
+
+# alien_0['color'] = 'yellow'
+# print('The alien is ' + alien_0['color'] + '.')
+
