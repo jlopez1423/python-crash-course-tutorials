9970bceb11be11c42d1b5c3ee5c4ac3b7dff2ddd
Jose <jalopez1423@gmail.com>
2018-09-20 20:39:35 -0700

    more dictionary exercises

---
+++ chapter-6-exercises/favoriteNumbers.py
@@ -0,0 +1,12 @@
+favorite_numbers = {
+    'Jose': 20,
+    'Serena': 7,
+    'Ginger': 28,
+    'Melany': 6,
+    'Petunia': 5
+}
+print('Jose ' + str(favorite_numbers['Jose']))
+print('Serena ' + str(favorite_numbers['Serena']))
+print('Ginger ' + str(favorite_numbers['Ginger']))
+print('Melany ' + str(favorite_numbers['Melany']))
+print('Petunia ' + str(favorite_numbers['Petunia']))
---
+++ chapter-6-exercises/person.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'Jose',
+    'last_name': 'Lopez',
+    'age': 26,
+    'city': 'San Luis Obispo'
+}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
