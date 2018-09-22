224280982b9996bae31d5c3a206d4ef3ffd39f63
Jose <jalopez1423@gmail.com>
2018-09-21 21:07:05 -0700

    more dictionaries examples

---
+++ chapter-6-examples/favorite_languages.py
@@ -0,0 +1,14 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+          language.title() + ".")
+
+# Looping through all the keys in a dictionary
+for name in favorite_languages.keys():
+    print(name.title())
