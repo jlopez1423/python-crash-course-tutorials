c09fc108c9fbcf1e0816794a0797493334f13f40
Jose <jalopez1423@gmail.com>
2018-09-24 20:13:40 -0700

    more dictionary examples/exercises

---
+++ chapter-6-exercises/people.py
@@ -0,0 +1,25 @@
+person_1 = {
+    'first_name': 'Jose',
+    'last_name': 'Lopez',
+    'age': 26,
+    'city': 'Atascadero'
+}
+
+person_2 = {
+    'first_name': 'Serena',
+    'last_name': 'Watton',
+    'age': 26,
+    'city': 'Atascadero'
+}
+
+person_3 = {
+    'first_name': 'Melany',
+    'last_name': 'Lopez',
+    'age': 6,
+    'city': 'Greenfield'
+}
+
+people = [person_1, person_2, person_3]
+
+for person in people:
+    print(person)
---
+++ chapter-6-exercises/pets.py
@@ -0,0 +1,18 @@
+ginger = {
+    'type': 'dog',
+    'owner': 'Serena Watton'
+}
+
+petunia = {
+    'type': 'dog',
+    'owner': 'Chelsy Lopez'
+}
+
+yogi = {
+    'type': 'dog',
+    'owner': 'Jose Lopez'
+}
+
+pets = [ginger, petunia, yogi]
+for pet in pets:
+    print(pet)
