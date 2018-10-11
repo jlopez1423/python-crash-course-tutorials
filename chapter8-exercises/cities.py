95dde60060df512038566fe43def59d89e12a211
Jose <jalopez1423@gmail.com>
2018-10-10 20:33:19 -0700

    more examples

---
+++ chapter8-exercises/cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country='unkown'):
+    print(city.title() + ' is in ' + country.title())
+
+describe_city('King City', 'usa')
+describe_city('Guanajuato', 'Mexico')
+describe_city('blah blah')
---
+++ chapter8-exercises/tshirt.py
@@ -0,0 +1,9 @@
+def make_shirt(size="Large", message="I love python"):
+    print("The size of the shirt is " + size + " with message " + message)
+
+
+make_shirt('Large', 'Dobies are great')
+make_shirt(message="I'm allergic to water", size="Small")
+make_shirt('Large')
+make_shirt('Medium')
+make_shirt('Small', 'Any message')
