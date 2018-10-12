15d395c57c4db491d60f249ce3153a28bdea36fc
Jose <jalopez1423@gmail.com>
2018-10-11 21:31:54 -0700

    functions and objects

--- chapter8-exercises/cities.py
+++ chapter8-exercises/cities.py
@@ -1,6 +1,7 @@
 def describe_city(city, country='unkown'):
     print(city.title() + ' is in ' + country.title())
 
+
 describe_city('King City', 'usa')
 describe_city('Guanajuato', 'Mexico')
 describe_city('blah blah')
