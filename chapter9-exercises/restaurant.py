7bbad3c92fb31171bbebd55fbd1d305dd9cb1266
Jose <jalopez1423@gmail.com>
2018-10-28 20:07:55 -0700

    more exercises

--- chapter9-exercises/restaurant.py
+++ chapter9-exercises/restaurant.py
@@ -3,6 +3,7 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(self.restaurant_name.title() + " serves " + self.cuisine_type.title() + " food.")
@@ -10,10 +11,23 @@ class Restaurant:
     def open_restaurant(self):
         print(self.restaurant_name.title() + " is now open.")
 
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+    def increment_number_served(self, daily_count):
+        self.number_served += daily_count
+
+# Exercise 9-4
+restaurant = Restaurant("Some Rest", "Chinese")
+restaurant.set_number_served(5489)
+restaurant.increment_number_served(89)
+print("Number of customers served: " + str(restaurant.number_served))
+
 
 my_restaurant = Restaurant("Jose's Tacos", "Mexican")
 gingers_restaurant = Restaurant("Gingers Truffles", "Modern American")
 serenas_fixings = Restaurant("Serenas fixings", "Fusion")
+
 
 print("\nRestaurant name: " + my_restaurant.restaurant_name.title())
 print("Restaurant type: " + my_restaurant.cuisine_type.title())
--- chapter9-exercises/users.py
+++ chapter9-exercises/users.py
@@ -5,6 +5,7 @@ class Users:
         self.last_name = last_name
         self.age = age
         self.email = email
+        self.login_attempts = 0
 
     def describe_user(self):
         print("Users Name: " + self.first_name + " " + self.last_name)
@@ -14,7 +15,21 @@ class Users:
     def greet_user(self):
         print("Welcome " + self.first_name + " " + self.last_name)
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 
 my_self = Users('Jose', 'Lopez', '26', 'jlopez@place.com')
 my_self.describe_user()
 my_self.greet_user()
+my_self.increment_login_attempts()
+my_self.increment_login_attempts()
+my_self.increment_login_attempts()
+my_self.increment_login_attempts()
+my_self.increment_login_attempts()
+print("Amount of login attempts: " + str(my_self.login_attempts))
+my_self.reset_login_attempts()
+print("Amount of login attempts: " + str(my_self.login_attempts))
