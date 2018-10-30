5f3ec267b0517db2ddbfc015bcd7929d72d3907b
Jose <jalopez1423@gmail.com>
2018-10-29 20:43:53 -0700

    examples inheritance

--- chapter9-examples/car.py
+++ chapter9-examples/car.py
@@ -31,6 +31,17 @@ class Car:
         self.odometer_reading += mileage
 
 
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class"""
+        super().__init__(make, model, year)
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 
