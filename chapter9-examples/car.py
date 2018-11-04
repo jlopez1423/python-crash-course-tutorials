0c17b2f61d22e72eb2ad59f837ef06bf8bd9705d
Jose <jalopez1423@gmail.com>
2018-11-03 21:42:19 -0700

    more examples

--- chapter9-examples/car.py
+++ chapter9-examples/car.py
@@ -30,6 +30,15 @@ class Car:
     def increment_odometer(self, mileage):
         self.odometer_reading += mileage
 
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes"""
+        self.batter_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.batter_size) + "-kWh battery.")
 
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles."""
@@ -37,10 +46,17 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
+        # self.battery_size = 70
+        self.battery = Battery()
+
+    # def describe_battery(self):
+    #     """Print a statement describing the battery size"""
+    #     print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
